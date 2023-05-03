7bfd603e7a4597aeb088507821d6d1194e26727e
VvsJaemin <djd453216@nate.com>
2023-05-03 22:47:16 +0900

    자동 구성 설정 및 라이브러리

--- memory-v2/src/main/java/memory/MemoryAutoConfig.java
+++ memory-v2/src/main/java/memory/MemoryAutoConfig.java
@@ -1,18 +1,14 @@
-package hello.config;
+package memory;
 
-import hello.memory.MemoryCondition;
-import hello.memory.MemoryController;
-import hello.memory.MemoryFinder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@Conditional(MemoryCondition.class) // true일경우에만 빈 등록 -> 아래 메서드 실행
-@ConditionalOnProperty(name = "memory", havingValue = "on") //
-public class MemoryConfig {
+@ConditionalOnProperty(name = "memory", havingValue = "on")
+public class MemoryAutoConfig {
 
+    @Bean
     public MemoryController memoryController() {
         return new MemoryController(memoryFinder());
     }
