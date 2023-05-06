3e7dabf7d17a602a151e3219176cb97fe3b62e1b
VvsJaemin <djd453216@nate.com>
2023-05-06 10:46:09 +0900

    스프링부트 우선순위

--- external/src/main/java/hello/EnvironmentCheck.java
+++ external/src/main/java/hello/EnvironmentCheck.java
@@ -20,6 +20,7 @@ public class EnvironmentCheck {
         String url = env.getProperty("url");
         String username = env.getProperty("username");
         String password = env.getProperty("password");
+        String new2 = env.getProperty("new2");
         log.info("env url={}", url);
         log.info("env username={}", username);
         log.info("env password={}", password);
